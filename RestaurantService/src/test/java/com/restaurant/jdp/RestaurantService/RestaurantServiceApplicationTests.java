5a651604493f21a968ed06047fbcd5862c976454
M V K <krishna.mugala9@gmail.com>
2025-02-26 16:43:25 +0530

    Initial Commit . Created project Structure.

---
+++ RestaurantService/src/test/java/com/restaurant/jdp/RestaurantService/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.jdp.RestaurantService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
